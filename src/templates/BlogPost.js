4f5c434ab462cd8c27268503f2230f7d2754160a
imranmollajoy <imranmollajoy@gmail.com>
2022-05-12 20:09:15 +0600

    added richtext render option

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -4,9 +4,55 @@ import { graphql } from "gatsby"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Layout } from "../components/ui"
-
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 const BlogPost = ({ data }) => {
   const post = data.contentfulBlogPost
+  const renderRichTextOptions = {
+    renderMark: {
+      [MARKS.BOLD]: text => <strong>{text}</strong>,
+      [MARKS.UNDERLINE]: text => <u>{text}</u>,
+      [MARKS.ITALIC]: text => <em>{text}</em>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => (
+        <Text
+          textAlign="left"
+          my={4}
+          fontSize={{ base: "sm", sm: "md", md: "lg" }}
+        >
+          {children}
+        </Text>
+      ),
+      [BLOCKS.HEADING_1]: (node, children) => (
+        <Heading as="h1" size="xl">
+          {children}
+        </Heading>
+      ),
+      [BLOCKS.HEADING_2]: (node, children) => (
+        <Heading as="h2" size="lg">
+          {children}
+        </Heading>
+      ),
+      [BLOCKS.HEADING_3]: (node, children) => (
+        <Heading as="h3" size="md">
+          {children}
+        </Heading>
+      ),
+      [BLOCKS.HEADING_4]: (node, children) => (
+        <Heading as="h4" size="md">
+          {children}
+        </Heading>
+      ),
+      [BLOCKS.HEADING_5]: (node, children) => (
+        <Heading as="h5" size="sm">
+          {children}
+        </Heading>
+      ),
+      [BLOCKS.HEADING_6]: (node, children) => (
+        <Heading as="h6">{children}</Heading>
+      ),
+    },
+  }
   return (
     <Layout>
       <Box position="relative">
@@ -30,7 +76,7 @@ const BlogPost = ({ data }) => {
               {post.category}
             </Badge>
             <Heading my={4}>{post.title}</Heading>
-            <Box>{renderRichText(post.body)}</Box>
+            <Box>{renderRichText(post.body, renderRichTextOptions)}</Box>
           </Box>
         </Container>
       </Box>
