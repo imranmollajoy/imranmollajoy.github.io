d4653edfd0488e79bfbc0dadfd71608a3b9419fe
imranmollajoy <imranmollajoy@gmail.com>
2022-05-20 18:16:36 +0600

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,36 @@
 import React from "react"
-import { Heading, Text } from "@chakra-ui/react"
+import { Container, Heading, Text, Box, Button } from "@chakra-ui/react"
 import { Link } from "gatsby"
 import { Layout } from "../components/ui"
 const NotFoundPage = () => {
   return (
     <Layout>
-      <Heading as="h1" fontSize="3xl">
-        Huh, weird
-      </Heading>
-      <Heading as="h2" fontSize="md">
-        This page doesn't exist
-      </Heading>
-      <Text as="p">
-        You can always go back to the <Link to="/">home page</Link>
-      </Text>
+      <Box>
+        <Container
+          maxW="container.xl"
+          minH="100vh"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <Heading as="h1" size="4xl" color="text">
+            Huh
+            <Heading as="span" size="4xl">
+              !
+            </Heading>{" "}
+            Weird
+          </Heading>
+          <Heading as="h2" size="xl" color="text">
+            This page doesn't exist
+          </Heading>
+          <Text as="p" size="xl" mb="4">
+            However, you can always go back to the home page
+          </Text>
+          <Link to="/">
+            <Button>Go to Home Page</Button>
+          </Link>
+        </Container>
+      </Box>
     </Layout>
   )
 }
