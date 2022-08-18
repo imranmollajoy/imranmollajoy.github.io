b1ad867dacd1d4232a68108d60c707689a1f8810
imranmollajoy <imranmollajoy@gmail.com>
2022-08-18 11:04:04 +0600

    hero curve

--- src/lib/components/sections/home/Hero.svelte
+++ src/lib/components/sections/home/Hero.svelte
@@ -1,4 +1,9 @@
+<script>
+	import CURVE from '$lib/images/Curve.svg';
+</script>
+
 <section>
+	<img src={CURVE} alt="" />
 	<div class="container">
 		<h3>Hi there, hello</h3>
 		<h1>I am an <span class="accent">Web Developer</span></h1>
@@ -10,10 +15,27 @@
 </section>
 
 <style>
+	section {
+		position: relative;
+		overflow-x: hidden;
+		margin: 0;
+	}
 	.container {
 		min-height: 95vh;
 		display: flex;
 		justify-content: center;
 		flex-direction: column;
+	}
+	img {
+		position: absolute;
+		z-index: -1;
+		top: 0;
+		right: -5%;
+	}
+	/* media for phone */
+	@media (max-width: 768px) {
+		img {
+			right: -30%;
+		}
 	}
 </style>
