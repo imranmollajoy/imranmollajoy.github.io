84c77acd847f346b8d48104105d15baf6ba630cf
imranmollajoy <imranmollajoy@gmail.com>
2022-08-21 23:31:30 +0600

    sitempa

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ then
   echo "Which folder do you want to deploy to GitHub Pages?"
   exit 1
 fi
-pnpm run build && pnpm run deploy && git pull && git add -f --all build/ &&git subtree push --prefix $1 origin gh-pages
+pnpm run build && pnpm run postbuild && pnpm run deploy && git pull && git add -f --all build/ &&git subtree push --prefix $1 origin gh-pages
