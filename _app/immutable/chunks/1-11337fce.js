bd4cf95a0feadec9936479d03a391047e48830d2
Imran Molla Joy <imranmollajoy@gmail.com>
2022-08-21 21:35:44 +0600

    Updates

---
+++ _app/immutable/chunks/1-11337fce.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-69608359.js";import"./index-ea49385d.js";import"./singletons-279d7da1.js";export{r as component};
--- _app/immutable/chunks/1-39ae71f3.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-3eccb503.js";import"./index-ea49385d.js";import"./singletons-50cb6bc3.js";export{r as component};
