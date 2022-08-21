34d9ebd57965d5b12c2d66334853108a88ebfa3c
imranmollajoy <imranmollajoy@gmail.com>
2022-08-22 00:08:27 +0600

    progressbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,25 @@
 <script>
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
 	import '$lib/styles/normalize.css';
 	import '$lib/styles/skeleton.css';
 	import '$lib/styles/shared.css';
-	import '$lib/styles/simple-grid.css';
-	export let data;
+	// import '$lib/styles/simple-grid.css';
 	const portfolioMarkdownImages = import.meta.glob('../data/portfolios/images/*.{png,jpg}');
 	const postMarkdownImages = import.meta.glob('../data/blogs/**/*.{png,jpg}');
+	NProgress.configure({
+		// Full list: https://github.com/rstacruz/nprogress#configuration
+		minimum: 0.16,
+		showSpinner: true
+	});
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <slot />
