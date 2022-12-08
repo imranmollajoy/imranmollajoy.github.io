88460cb9c2cf50d4fee469c84778575170832d77
Imran Molla Joy <imranmollajoy@gmail.com>
2022-12-08 22:01:21 +0600

    check

--- push.sh
+++ push.sh
@@ -6,4 +6,4 @@ then
 fi
 #pnpm run build && pnpm run postbuild && pnpm run deploy && git pull && git add -f --all build/ &&git subtree push --prefix $1 origin gh-pages
 # pnpm run build && pnpm run deploy && git pull && git add -f --all build/ &&git subtree push --prefix $1 origin gh-pages
- git pull && git add -f --all build/ &&git subtree push --prefix $1 origin gh-pages
+git pull && git add -f --all build/ &&git subtree push --prefix $1 origin gh-pages
