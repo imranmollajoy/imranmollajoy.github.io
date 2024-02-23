bd6ae3e19f02a8aa66bf1d664d651c47de462dc2
Imran Molla Joy <imranmollajoy@gmail.com>
2024-02-23 14:27:49 +0600

    prettier story

--- src/lib/templates/story.svelte
+++ src/lib/templates/story.svelte
@@ -3,12 +3,32 @@
 
 	export let title;
 	export let tags;
+	export let date;
+	export let updates;
+	export let description;
+
+	const OPTIONS = { weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric' };
+
+	function getBDDate(date) {
+		return new Date(date).toLocaleDateString('bn-BD', OPTIONS);
+	}
 </script>
 
-<Seo {title} keywords={tags} />
+<Seo {title} {description} keywords={tags} />
 <div class="p-4 max-w-2xl mx-auto lg:max-w-3xl">
 	<article class="prose max-w-none lg:prose-xl dark:prose-invert" id="toc-target">
-		<h1 class="mt-12">{title}</h1>
+		<div class="mt-12 space-y-0">
+			<p class="text-neutral-500">{description}</p>
+			<h1>{title}</h1>
+			<p>ইমরান মোল্লা জয়</p>
+			<p>প্রথম প্রকাশ: {getBDDate(date)}</p>
+		</div>
 		<slot />
+		{#if updates}
+			<hr />
+			{#each updates as update, index (update)}
+				সংস্করণ {(index + 2).toLocaleString('bn-BD')}: {getBDDate(update)}
+			{/each}
+		{/if}
 	</article>
 </div>
